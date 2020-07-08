07e21bf238191bcba1356d45527b2daa3282f919
PAKD <setevieru@yandex.ru>
2020-07-08 18:08:10 +0300

    add filter

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -17,7 +17,9 @@ export default class App extends Component {
       this.createTodoItem('Drink Coffee'),
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch'),
-    ]
+    ],
+    term: '',
+    filter: 'active' //active , all, done
   }
 
   createTodoItem(label) {
@@ -81,12 +83,46 @@ export default class App extends Component {
         todoData: this.toggleProperty(todoData, id, 'done')
       };
     });
+  };
 
+  onSearchChange = (term) => {
+    this.setState({ term })
+  };
+
+  onFilterChange = (filter) => {
+    this.setState({
+      filter
+    })
+  }
+
+  search(items, term) {
+
+    if( term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
+    });
+  }
+
+  filter(items, filter) {
+    switch(filter) {
+      case 'all': 
+        return items;
+      case 'active': 
+        return items.filter((item) => !item.done)
+      case 'done':
+        return items.filter((item) => item.done);
+        default: 
+        return items; 
+    }
   }
 
   render() {
 
-    const { todoData } = this.state;
+    const { todoData, term, filter } = this.state;
+    const visibleItems = this.filter(this.search(todoData, term), filter)
 
     const doneCount = todoData
       .filter((el) => el.done).length;
@@ -95,12 +131,17 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
+          <SearchPanel 
+          onSearchChange={this.onSearchChange}
+          />
+          <ItemStatusFilter 
+          filter={filter}
+          onFilterChange={this.onFilterChange}
+          />
         </div>
 
         <TodoList
-          todos={todoData}
+          todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
--- test.js
+++ test.js
@@ -1,1 +1,40 @@
 
+const arr = [
+  { name: 'first', timeout: 3000 },
+{ name: 'second', timeout: 5000 },
+{ name: 'third', timeout: 2000 },
+];
+
+
+
+async function run (arr) {
+
+  for(y of arr) {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(y.name), y.timeout)
+    });
+    let result = await promise;
+    console.log(result)
+  }
+  
+}
+
+console.log(run(arr))
+
+
+
+const run = async (_arr) => {
+  for (x of _arr) {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(x.name), x.timeout)
+    });
+    let result = await promise;
+    console.log(result);
+  }
+};
+const arr = [
+  { name: 'first', timeout: 3000 },
+  { name: 'second', timeout: 5000 },
+  { name: 'third', timeout: 2000 },
+];
+run(arr);
